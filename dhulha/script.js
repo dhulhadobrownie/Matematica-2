5b628fa22e8181a1c911fe0b137b4a63d49daca9
dhu <146989266+dhulhadobrownie@users.noreply.github.com>
2025-05-08 07:26:00 -0300

    Update script.js

--- dhulha/script.js
+++ dhulha/script.js
@@ -29,3 +29,12 @@ botaoDeAcessibilidade.addEventListener('click', function (){
 const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
 const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
 botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"></script>
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
